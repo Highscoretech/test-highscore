e62f23d91f20855f277140b66e98f8737f669369
Highscoretech <highscoretechng@gmail.com>
2025-06-25 11:27:12 +0100

    Add netlify.toml for deployment and fix potential routing issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // This tells Next.js to export to static HTML/CSS/JS
+//   output: 'export', // This tells Next.js to export to static HTML/CSS/JS
 };
 module.exports = nextConfig;
